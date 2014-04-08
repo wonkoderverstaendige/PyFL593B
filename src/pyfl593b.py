4658f4d8f32af39bbb1ee9762576276a8beda6b6
Ronny Eichler <ronny.eichler@gmail.com>
2014-04-08 16:40:36 +0200

    pyfl593b.py as future command line tool

--- src/pyfl593b.py
+++ src/pyfl593b.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on 4/5/14 3:31 AM 2013
@@ -8,11 +9,20 @@ GUI controlling PyFL593B interface instance
 
 __version__ = '0.0.1'
 
+import logging
 import sys
 if sys.hexversion > 0x03000000:
     raise EnvironmentError('Python 3 not supported.')
-
 from core.fl593b import FL593B
-import ui
 
-dev = FL593B()
+
+def main(*args, **kwargs):
+    pass
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Command line parsing
+
+    main()

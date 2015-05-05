afa0ab28b53ba196a12ff9ca7dd2b905a3fc0ff4
Ronny Eichler <r.eichler@science.ru.nl>
2015-05-05 04:50:35 +0200

    Reflect single-byte size of codes in constants

--- PyFL593FL/core/constants.py
+++ PyFL593FL/core/constants.py
@@ -43,7 +43,7 @@ EXIT_REMOTE_ENABLE_STATE = False  # For safety reasons, disable on exit
 # DEVICE CONSTANTS
 VENDOR_ID = 0x1a45
 PRODUCT_ID = 0x2001
-DEV_TYPE = 0x0000
+DEV_TYPE = 0x00
 CONFIGURATION = 1
 MAX_CONN_RETRIES = 10
 EP_ADDR_OUT = 0x01
@@ -53,9 +53,9 @@ EP_PACK_IN = 21  # length bytes returned
 
 # CHANNELS
 MAX_NUM_CHAN = 2
-CHAN_STATUS = 0x0000
-CHAN_LD1 = 0x0001
-CHAN_LD2 = 0x0002
+CHAN_STATUS = 0x00
+CHAN_LD1 = 0x01
+CHAN_LD2 = 0x02
 CHANNEL_DICT = {
     'STATUS': CHAN_STATUS,
     'LD1': CHAN_LD1,
@@ -64,10 +64,10 @@ CHANNEL_DICT = {
 CHANNEL_DICT_REV = {v: k for k, v in CHANNEL_DICT.iteritems()}
 
 # OpTypes
-TYPE_READ = 0x0001  # return OpCode quantity to host
-TYPE_WRITE = 0x0002  # write OpCode  quantity to device
-TYPE_MIN = 0x0003  # return minimum of OpCode quantity to host
-TYPE_MAX = 0x0004  # return maximum of OpCode quantity to host
+TYPE_READ = 0x01  # return OpCode quantity to host
+TYPE_WRITE = 0x02  # write OpCode  quantity to device
+TYPE_MIN = 0x03  # return minimum of OpCode quantity to host
+TYPE_MAX = 0x04  # return maximum of OpCode quantity to host
 OP_TYPE_DICT = {
     "READ": TYPE_READ,
     "WRITE": TYPE_WRITE,
@@ -78,29 +78,29 @@ OP_TYPE_DICT_REV = {v: k for k, v in OP_TYPE_DICT.iteritems()}
 
 # General OpCodes
 # r = read, w = write
-CMD_MODEL = 0x0000  # r
-CMD_SERIAL = 0x0001  # rw
-CMD_FWVER = 0x0002  # r
-CMD_DEVTYPE = 0x0003  # r
-CMD_CHANCT = 0x0004  # r
-CMD_IDENTIFY = 0x0005  # rw
-CMD_SAVE = 0x000C  # w
-CMD_RECALL = 0x000D  # w
-CMD_PASSWD = 0x000E  # rw
-CMD_REVERT = 0x000F  # w
+CMD_MODEL = 0x00  # r
+CMD_SERIAL = 0x01  # rw
+CMD_FWVER = 0x02  # r
+CMD_DEVTYPE = 0x03  # r
+CMD_CHANCT = 0x04  # r
+CMD_IDENTIFY = 0x05  # rw
+CMD_SAVE = 0x0C  # w
+CMD_RECALL = 0x0D  # w
+CMD_PASSWD = 0x0E  # rw
+CMD_REVERT = 0x0F  # w
 # Specific OpCodes (codes > 0x10 device specific)
 # r = read, w = write, mm = min/max
-CMD_ALARM = 0x0010  # r, Alarm flags
-CMD_SETPOINT = 0x0011  # rwm, Setpoint, units depend on device MODE!
-CMD_LIMIT = 0x0012  # rwm, current limit (Ampere, applies for CC, CP and analog modulation!)
-CMD_MODE = 0x0013  # rw, feedback mode
-CMD_TRACK = 0x0014  # rw, tracking configuration (independent: 0/parallel: 1)
-CMD_IMON = 0x0015  # current monitor
-CMD_PMON = 0x0016  # power monitor
-CMD_ENABLE = 0x0017  # rw, output enable. Additionally requires the output enable switch
+CMD_ALARM = 0x10  # r, Alarm flags
+CMD_SETPOINT = 0x11  # rwm, Setpoint, units depend on device MODE!
+CMD_LIMIT = 0x12  # rwm, current limit (Ampere, applies for CC, CP and analog modulation!)
+CMD_MODE = 0x13  # rw, feedback mode
+CMD_TRACK = 0x14  # rw, tracking configuration (independent: 0/parallel: 1)
+CMD_IMON = 0x15  # current monitor
+CMD_PMON = 0x16  # power monitor
+CMD_ENABLE = 0x17  # rw, output enable. Additionally requires the output enable switch
 # on the board to be set to EN, and the NT pin to be pulled LOW
-CMD_RPD = 0x0019  # rwm, kOhm, photodiode feedback resistor for specified channel
-CMD_CAL_ISCALE = 0x00E2  # rw, current monitor calibration scaling value for selected channel
+CMD_RPD = 0x19  # rwm, kOhm, photodiode feedback resistor for specified channel
+CMD_CAL_ISCALE = 0xE2  # rw, current monitor calibration scaling value for selected channel
 OP_CODE_DICT = {
     'MODEL': CMD_MODEL,
     'SERIAL': CMD_SERIAL,
@@ -160,16 +160,16 @@ OP_CODE_DICT_REV = {v: k for k, v in OP_CODE_DICT.iteritems()}
 
 # ALARM FLAGS
 NUM_ALARMS = 10
-ALARM_OUT = 0x0000  # output status. 0: off, 1: on; equals XEN*LEN*REN
-ALARM_XEN = 0x0001  # external enable flag, 0: J102 floating or HIGH, 1: LOW
-ALARM_LEN = 0x0002  # local enable flag, 0: S100 enabled, 0: disabled
-ALARM_REN = 0x0003  # remote enable flag, enable command state
-ALARM_MODE1 = 0x0004  # feedback mode channel 1, 0: CC, 1: CP
-ALARM_MODE2 = 0x0005  # feedback mode channel 2, 0: CC, 1: CP
-ALARM_PARA = 0x0006  # parallel mode, state of track command, 0: independently
-ALARM_IDENT = 0x0007  # status identify flag, 0: inactive, 1: active
-ALARM_WRITE = 0x0008  # write to non-volatile memory in progress following SAVE
-ALARM_CALMODE = 0x0009  # 1: device is in calibration mode, entered with PASSWD and left with REVERT
+ALARM_OUT = 0000  # output status. 0: off, 1: on; equals XEN*LEN*REN
+ALARM_XEN = 0x01  # external enable flag, 0: J102 floating or HIGH, 1: LOW
+ALARM_LEN = 0x02  # local enable flag, 0: S100 enabled, 0: disabled
+ALARM_REN = 0x03  # remote enable flag, enable command state
+ALARM_MODE1 = 0x04  # feedback mode channel 1, 0: CC, 1: CP
+ALARM_MODE2 = 0x05  # feedback mode channel 2, 0: CC, 1: CP
+ALARM_PARA = 0x06  # parallel mode, state of track command, 0: independently
+ALARM_IDENT = 0x07  # status identify flag, 0: inactive, 1: active
+ALARM_WRITE = 0x08  # write to non-volatile memory in progress following SAVE
+ALARM_CALMODE = 0x09  # 1: device is in calibration mode, entered with PASSWD and left with REVERT
 ALARM_FLAG_DICT = {
     'OUT':    ALARM_OUT,  # output status. 0: off, 1: on; equals XEN*LEN*REN
     'XEN':    ALARM_XEN,  # external enable flag, 0: J102 floating or HIGH, 1: LOW
@@ -186,20 +186,20 @@ ALARM_FLAG_DICT_REV = {v: k for k, v in ALARM_FLAG_DICT.iteritems()}
 
 # FLAGS (instead of taking non-zero values as true, uses literal ASCII characters)
 # I assume all input gets fed through atoi, sscan or similar?
-FLAG_OFF = 0x0030  # ASCII 48, '0'
-FLAG_ON = 0x0031  # ASCII 49, '1'
+FLAG_OFF = 0x30  # ASCII 48, '0'
+FLAG_ON = 0x31  # ASCII 49, '1'
 
 # end codes
-ERR_OK = 0x0000  # no error
-ERR_DEVTYPE = 0x0001  # device type field incorrect
-ERR_CHANNEL = 0x0002  # channel number out of range
-ERR_OPTYPE = 0x0003  # op_type not supported
-ERR_NOTIMPL = 0x0004  # op_code not implemented
-ERR_PENDING = 0x0005  # command received, but not completed [ignore data]
-ERR_BUSY = 0x0006  # device currently busy, has not performed requested op
-ERR_DATA = 0x0007  # data field content invalid for op_code
-ERR_SAFETY = 0x0008  # requested op not within safety specs of config
-ERR_CALMODE = 0x0009  # requested op only available in calibration mode!
+ERR_OK = 0x00  # no error
+ERR_DEVTYPE = 0x01  # device type field incorrect
+ERR_CHANNEL = 0x02  # channel number out of range
+ERR_OPTYPE = 0x03  # op_type not supported
+ERR_NOTIMPL = 0x04  # op_code not implemented
+ERR_PENDING = 0x05  # command received, but not completed [ignore data]
+ERR_BUSY = 0x06  # device currently busy, has not performed requested op
+ERR_DATA = 0x07  # data field content invalid for op_code
+ERR_SAFETY = 0x08  # requested op not within safety specs of config
+ERR_CALMODE = 0x09  # requested op only available in calibration mode!
 END_CODE_DICT = {
     ERR_OK: "OK",
     ERR_DEVTYPE: "DEVTYPE",
